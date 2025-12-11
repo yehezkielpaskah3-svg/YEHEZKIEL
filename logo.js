2cda8aebbf0c7c5b45b4c2d984026b7cf238adfb
yehezkielpaskah3-svg <yehezkielpaskah3@gmail.com>
2025-12-11 13:06:03 +0700

    Add files via upload

---
+++ logo.js
@@ -0,0 +1,6 @@
+/* -------------------------
+    Cover image (menggunakan file eksternal)
+    PASTIKAN FILE 'unpam_cover.jpg' ADA DI FOLDER YANG SAMA!
+    ------------------------- */
+const imagePath = "unpam_cover.jpg";
+document.getElementById('loginCover').style.backgroundImage = `url('${imagePath}')`;
